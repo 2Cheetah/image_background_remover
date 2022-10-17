c2c4360d206535efcef490adf8e7d54f261e7164
2Cheetah <fckninja@gmail.com>
2022-10-17 17:09:39 +0800

    Amendments to the main.py file
    Added new feature - a user must specify a folder path with input images. Now the program creates output_pics folder if it doesn't exist

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from rembg import remove
 from PIL import Image
 from pathlib import Path
-from os import listdir
+from os import listdir, mkdir
 from os.path import splitext
 
 
-def remove_bg():
+def remove_bg(input_pics):
     list_of_extensions = ['.png', '.jpg']
-    all_files = [pic for pic in listdir('./input_pics') if splitext(pic)[1] in list_of_extensions]
-    # print(all_files)
+    all_files = [pic for pic in listdir(input_pics) if splitext(pic)[1] in list_of_extensions]
     output_filenames = [splitext(pic)[0]+'_wo_bg'+splitext(pic)[1] for pic in all_files]
     
     for index, pic in enumerate(all_files, start=1):
-        input_path_filename = Path('./input_pics/' + pic)
+        input_path_filename = Path(input_pics + '/' + pic)
         output_path_filename = Path('./output_pics/' + splitext(pic)[0] + '_wo_bg' + '.png')
         input_image = Image.open(input_path_filename)
         output_image = remove(input_image)
@@ -22,4 +21,7 @@ def remove_bg():
 
 
 if __name__ == '__main__':
-    remove_bg()
+    if 'output_pics' not in listdir():
+        mkdir('output_pics')
+    input_pics = input("Route to the folder with the pictures to process: ").strip().replace("'","")
+    remove_bg(input_pics)
