6d32fc1f8aa6ebccd7626e02855874aa962563a1
2Cheetah <fckninja@gmail.com>
2022-10-17 15:59:17 +0800

    Small typo amendments in main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def remove_bg():
     # print(all_files)
     output_filenames = [splitext(pic)[0]+'_wo_bg'+splitext(pic)[1] for pic in all_files]
     
-    for index, pic in enumerate(all_files):
+    for index, pic in enumerate(all_files, start=1):
         input_path_filename = Path('./input_pics/' + pic)
         # print(abspath(input_path_filename))
         output_path_filename = Path('./output_pics/' + splitext(pic)[0] + '_wo_bg' + '.png')
@@ -21,7 +21,8 @@ def remove_bg():
         output_image = remove(input_image)
         output_image.save(output_path_filename)
 
-        print(f'Completed: {index + 1}/{len(all_files)}')
+        print(f'Completed: {index}/{len(all_files)}')
+
 
 if __name__ == '__main__':
     remove_bg()
